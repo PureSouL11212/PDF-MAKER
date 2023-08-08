f4c069171d79a5f4319c9c5aaaaa96081ff927a7
sameerkhan78600 <sameerkhan4321087@gmail.com>
2023-08-08 17:40:21 +0530

    fsdfsafs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import {jsPDF} from "jspdf";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pdf';
+
+
+
+  @ViewChild('content', {static:false}) el!:ElementRef;
+  title = 'Angular Cli and pdf';
+  makepdf(){
+    let pdf= new jsPDF('p','pt','a4');
+    // pdf.text("HELLO SAMEER",10,10);
+    pdf.html(this.el.nativeElement,{
+      callback:(pdf)=>{
+        pdf.save("demo.pdf");}
+    });
+
+   
+  }
 }
